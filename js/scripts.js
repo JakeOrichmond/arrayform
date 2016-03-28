3f6a9f8384f98d98e6fc81f10bbcac6e42a8f342
jake richmond and nick arrasate <nick@>
2016-03-28 09:48:15 -0700

    add forms and 2 arrays to log info in different orders.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+
+  var array = [];
+
+  $('form').submit(function(event){
+
+    var info1 = $("#fav1").val();
+    var info2 = $("#fav2").val();
+    var info3 = $("#fav3").val();
+    var info4 = $("#fav4").val();
+    var info5 = $("#fav5").val();
+    var info6 = $("#fav6").val();
+
+    array.push(info1,info2,info3,info4,info5,info6);
+    console.log(array);
+
+    var array2 = []
+
+    array2.push(array[2], array[4], array[1]);
+
+    console.log(array2);
+    event.preventDefault();
+  });
+});
